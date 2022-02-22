cfb761acbdcef4156e3d4ae5bbefd99029209cf1
philippmossier <mossier.dev@gmail.com>
2022-02-22 12:55:01 +0100

    add clipboardWSL fix and colorschemes

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
 vim.cmd [[
 try
-  colorscheme tokyonight
+  " colorscheme tokyonight
+  " colorscheme gruvbox
+  " colorscheme onedark
+   colorscheme nightfox "nightfox,nordfox,dayfox,dawnfox,duskfox
+  " colorscheme darkplus
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
